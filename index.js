59325e87c34bd36b6e40928ec8a822dc5405a6ed
jeremymiru <mirujeremy@gmail.com>
2024-03-25 22:43:01 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+    for (const name of names) {
+      messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return messages;
+  }
+
+function countDown(number) {
+
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+
+  countDown(10);
